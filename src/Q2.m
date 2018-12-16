9f9dddc4e3927d9e40abee5a713b3b869a45b350
unknown <alexsah97@gmail.com>
2018-12-16 23:24:45 +0200

    Complete first generalization for Q2.2

--- src/Q2.m
+++ src/Q2.m
@@ -1,44 +1,32 @@
-%%Script for Question 2
+%% Initialization
+files = ['../dataset/Data_Eval_E_1.mat'; '../dataset/Data_Eval_E_2.mat'; '../dataset/Data_Eval_E_3.mat'; '../dataset/Data_Eval_E_4.mat'];
+
 allData=zeros(4,1440000);
+spike_Times = cell(4,1);
+spike_Class = cell(4,1);
 
-load('../dataset/Data_Eval_E_1.mat');
-allData(1,:)=data;
-spikeTimes1=spikeTimes;
-spikeClass1=spikeClass;
-
-load('../dataset/Data_Eval_E_2.mat');
-allData(2,:)=data;
-spikeTimes2=spikeTimes;
-spikeClass2=spikeClass;
-
-load('../dataset/Data_Eval_E_3.mat');
-allData(3,:)=data;
-spikeTimes3=spikeTimes;
-spikeClass3=spikeClass;
-
-load('../dataset/Data_Eval_E_4.mat');
-allData(4,:)=data;
-spikeTimes4=spikeTimes;
-spikeClass4=spikeClass;
+for i=1:1:4
+    load(files(i,:));
+    allData(i,:) = data;
+    spike_Times{i} = spikeTimes;
+    spike_Class{i} = spikeClass;
+end
+%change name for more intuition & clear temp variables
+spikeTimes = spike_Times;   
+spikeClass = spike_Class;
+clear spike_Times spike_Class;
 
 k=@(sigma)(1.861248757651653+0.250156158913673./sigma-0.008006893531367./(sigma.*sigma)-2.410871628915119e-05/(sigma.*sigma.*sigma));
 
 %% Q2.1
 sigmas=zeros(4,1);
 measuredNumSpikes=zeros(4,1);
-for i=1:4
+for i=1:1:4
     sigmas(i)=median(abs(allData(i,:)))/0.6745;
-    %sigmas(i)=std(allData(i,:));
     T=k(sigmas(i))*sigmas(i);
     previousMeasuredSpike=0;
     for m=1:1:1440000
-       if (m>1 && (allData(i,m))>T && (allData(i,m-1))<=(allData(i,m)) && (allData(i,m))>=(allData(i,m)) && previousMeasuredSpike==0)
-           measuredNumSpikes(i)=measuredNumSpikes(i)+1;
-           previousMeasuredSpike=m;
-       elseif (m==1 && (allData(i,m))>T && (allData(i,m))>=(allData(i,m)) && previousMeasuredSpike==0)
-           measuredNumSpikes(i)=measuredNumSpikes(i)+1;
-           previousMeasuredSpike=m;    
-       elseif (m==1440000 && (allData(i,m))>T && (allData(i,m-1))<=(allData(i,m)) && previousMeasuredSpike==0)
+       if ((allData(i,m))>T && previousMeasuredSpike==0)
            measuredNumSpikes(i)=measuredNumSpikes(i)+1;
            previousMeasuredSpike=m;
        elseif (previousMeasuredSpike~=0 && (allData(i,m))<T)
@@ -46,11 +34,13 @@ for i=1:4
        end
     end
 end
-%%
-spikeTimesEst_1=zeros(measuredNumSpikes(1),1);
-spikeTimesEst_2=zeros(measuredNumSpikes(2),1);
-spikeTimesEst_3=zeros(measuredNumSpikes(3),1);
-spikeTimesEst_4=zeros(measuredNumSpikes(4),1);
+
+%spikeTimesEst initialization
+spikeTimesEst = cell(4,1);
+for i=1:1:4
+    spikeTimesEst{i} = zeros(measuredNumSpikes(i),1);
+end
+
 for i=1:4
     T=k(sigmas(i))*sigmas(i);
     previousMeasuredSpike=0;
@@ -58,15 +48,7 @@ for i=1:4
     for m=1:1:1440000
        if ((allData(i,m))>=T && previousMeasuredSpike==0)
            count=count+1;
-           if(i==1)
-               spikeTimesEst_1(count)=m;
-           elseif(i==2)
-               spikeTimesEst_2(count)=m;
-           elseif(i==3)
-               spikeTimesEst_3(count)=m;
-           elseif(i==4)
-               spikeTimesEst_4(count)=m;
-           end
+           spikeTimesEst{i}(count)=m;
            previousMeasuredSpike=m;
        elseif (previousMeasuredSpike~=0 && (allData(i,m))<T)
            previousMeasuredSpike=0;
@@ -76,31 +58,32 @@ end
 
 
 %% Q2.2
-
-SpikesEst_1=zeros(length(spikeTimesEst_1), 64);
-SpikesEst_2=zeros(length(spikeTimesEst_2), 64);
-SpikesEst_3=zeros(length(spikeTimesEst_3), 64);
-SpikesEst_4=zeros(length(spikeTimesEst_4), 64);
-
-centers1=zeros(length(spikeTimesEst_1),1);
-for i=1:1:length(spikeTimesEst_1)
-   k=spikeTimesEst_1(i);
-   l=0;
-   while (allData(1, k-l)<=allData(1, k-l+1))
-        l=l+1;
-   end
-   t=0;
-   while ((allData(1, k+t)>=allData(1, k) && allData(1, k+t)>=allData(1, k+t-1)))
-        t=t+1;
-   end
-   if (allData(1, k-l)<=-allData(1, k))
-       centers1(i)=k-l;
-   else
-       centers1(i)=k+t;
-   end
-   SpikesEst_1(i,:)=allData(1, centers1(i)-31:centers1(i)+32);
+%spikeTimesNewEst_1=zeros(measuredNumSpikes(1),1);
+spikesEst = cell(4,1);
+for j=1:1:4
+    spikesEst{j} = zeros(length(spikeTimesEst{j}),64);
+    centers=zeros(length(spikeTimesEst{j}),1);
+    for i=1:1:length(spikeTimesEst{j})
+       k=spikeTimesEst{j}(i);
+       l=0;
+       while (allData(1, k-l)<=allData(1, k-l+1))
+            l=l+1;
+       end
+       t=0;
+       while ((allData(1, k+t)>=allData(1, k) && allData(1, k+t)>=allData(1, k+t-1)))
+            t=t+1;
+       end
+       if (allData(1, k-l)<=-allData(1, k))
+           centers(i)=k-l;
+       else
+           centers(i)=k+t;
+       end
+       spikesEst{j}(i,:)=allData(1, centers(i)-31:centers(i)+32);
+       %spikeTimesNewEst_1(i) = centers(i);
+    end
+    figure()
+    plot(1:1:64, spikesEst{j}(:,:));
 end
-plot(1:1:64, SpikesEst_1(:,:));
 
 
 %% Q2.3
